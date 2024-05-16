049e9eff92a8f77e82cf00c23398afb2f6c08e3b
himanshu-khati <himanshukhati94@gmail.com>
2024-05-16 07:48:57 +0530

    hk:tutorial

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./styles/app.scss";
+
+function App() {
+  return (
+    <>
+      <div className="productCard">
+        hello
+        <br />
+        <a href="">Link</a>
+        <br />
+        <button>i am a button</button>
+        <br />
+        <p>Hello u have button css</p>
+        <p className="p1">Hello u have button css</p>
+        <p className="p2">Hello u have button css</p>
+        <p className="p3">Hello u have button css</p>
+        <p className="p4">Hello u have button css</p>
+        <p className="p5">Hello u have button css</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
